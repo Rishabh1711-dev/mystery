6897bfb8f364b3f8f55ce4e53e9ab94df939a07e
Rishabh1711-dev <rishabhbhardwaj0906@gmail.com>
2026-06-02 03:59:41 +0530

    change

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,7 +1,7 @@
 import dbConnect from '@/lib/dbConnect';
 import UserModel from '@/model/User';
 import bcrypt from 'bcryptjs';
-//import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
+import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
 
 export async function POST(request: Request) {
   await dbConnect();
@@ -60,26 +60,21 @@ export async function POST(request: Request) {
 
       await newUser.save();
     }
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
 
-    // Send verification email
-    // const emailResponse = await sendVerificationEmail(
-    //   email,
-    //   username,
-    //   verifyCode
-    // );
-    // if (!emailResponse.success) {
-    //   return Response.json(
-    //     {
-    //       success: false,
-    //       message: emailResponse.message,
-    //     },
-    //     { status: 500 }
-    //   );
-    // }
-    const emailResponse = {
-  success: true,
-  message: "Email verification skipped",
-};
 
     return Response.json(
       {
